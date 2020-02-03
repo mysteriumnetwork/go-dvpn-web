e0d05a1a6a2362c94d141ef15fc445ac2650db21
Tadas Krivickas <tadas.krivickas@gmail.com>
2020-02-03 13:39:14 +0200

    Store tools in go.mod

--- go.mod
+++ go.mod
@@ -6,5 +6,7 @@ require (
 	github.com/magefile/mage v1.9.0
 	github.com/mholt/archiver/v3 v3.3.0
 	github.com/mysteriumnetwork/go-ci v0.0.0-20200121125840-b99aac3d815c
+	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
+	github.com/shurcooL/vfsgen v0.0.0-20181202132449-6a9ea43bcacd
 	gopkg.in/src-d/go-git.v4 v4.12.0 // indirect
 )
---
+++ tools.go
@@ -0,0 +1,24 @@
+// +build tools
+
+/*
+ * Copyright (C) 2020 The "MysteriumNetwork/go-dvpn-web" Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dvpnweb
+
+import (
+	_ "github.com/shurcooL/vfsgen"
+)
